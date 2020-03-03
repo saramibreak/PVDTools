f2cde6af2b3d990eef1790c508f2eaf14ae6ce6c
saramibreak <upswbsbj_20150526@yahoo.co.jp>
2020-03-03 23:08:06 +0900

    - fixed: ppm (truly supported color and xp disc)

--- PVDTools/PVDTools.cpp
+++ PVDTools/PVDTools.cpp
@@ -5,11 +5,14 @@ This program is distributed in the hope that it will be useful, but WITHOUT ANY 
 You should have received a copy of the GNU General Public License along with this program; if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 
 Copyright (c) 2003 Tyler Knott, 2020 Sarami*/
+#pragma warning(disable:4514 4710 4711)
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-#pragma warning(disable:4996)
+#pragma warning(disable:4996 5045)
+
+#define UNREFERENCED_PARAMETER(P) (P)
 
 int split16(char* rawfile)
 {
@@ -63,7 +66,7 @@ int split8()
 	foutr = fopen("input-raw-right2.raw", "wb");
 	fseek(fin, 0, 2);
 	finSize = ftell(fin);
-	printf("File size: %d\n", finSize);
+//	printf("File size: %d\n", finSize);
 	fseek(fin, 0, 0);
 	for (finSize = finSize; finSize != 0; finSize = finSize - 2)
 	{
@@ -123,10 +126,10 @@ int addh()
 	fwrite(&bits, 2, 1, fout);
 	fwrite(dataa, 4, 1, fout);
 	fwrite(&finSize, 4, 1, fout);
-	buffer = (char*)malloc(finSize);
+	buffer = (char*)malloc((size_t)finSize);
 	if (buffer) {
-		fread(buffer, 1, finSize, fin);
-		fwrite(buffer, 1, finSize, fout);
+		fread(buffer, 1, (size_t)finSize, fin);
+		fwrite(buffer, 1, (size_t)finSize, fout);
 	}
 	fclose(fin);
 	fclose(fout);
@@ -134,19 +137,19 @@ int addh()
 	return 0;
 }
 
-int isHeader(char buf)
+int isHeader(unsigned char buf)
 {
 	int nRet = 0;
-	if (buf == (char)0xe1 || buf == (char)0xc3 || buf == (char)0xa5) {
+	if (buf == 0xe1 || buf == 0xc3 || buf == 0xa5) {
 		nRet = 1;
 	}
 	return nRet;
 }
 
-int isFooter(char buf)
+int isFooter(unsigned char buf)
 {
 	int nRet = 0;
-	if (buf == (char)0xd2 || buf == (char)0xb4 || buf == (char)0x96) {
+	if (buf == 0xd2 || buf == 0xb4 || buf == 0x96) {
 		nRet = 1;
 	}
 	return nRet;
@@ -154,9 +157,8 @@ int isFooter(char buf)
 
 size_t fseekToHeader(FILE* fp)
 {
-	int nRet = 0;
 	size_t size = 0;
-	char buf = 0;
+	unsigned char buf = 0;
 	while (!feof(fp) && !ferror(fp)) {
 		size += fread(&buf, 1, 1, fp);
 		if (isHeader(buf)) {
@@ -169,7 +171,7 @@ size_t fseekToHeader(FILE* fp)
 				}
 			}
 			if (0 < size && size < 1340) {
-				int size2 = 1340 - size - 1;
+				long size2 = (long)(1340 - size - 1);
 //				printf("size:%d,size2:%d\n", size, size2);
 				fseek(fp, size2, SEEK_CUR);
 				fread(&buf, 1, 1, fp);
@@ -196,7 +198,7 @@ int xtract()
 {
 	FILE * fin;
 	FILE * fout;
-	char buffer;
+	unsigned char buffer;
 //	unsigned char low;
 //	unsigned char high;
 	int times;
@@ -214,14 +216,14 @@ int xtract()
 //	size = size - 6402;
 	// skip pregap (if exists) and first header (1340 bytes)
 	size_t seeksize = fseekToHeader(fin);
-	printf("Seek size: %d\n", seeksize);
-	size = size - seeksize - 1340;
+	printf("Seek size: %zd\n", seeksize);
+	size = (long)(size - seeksize - 1340);
 
 	for (tt = size / 5880; tt > 0; tt--)
 	{
 		ts += 1;
 		printf("\rProcessing frame number %li", ts);
-		sprintf(fname, "%i.pgm", ts);
+		sprintf(fname, "%li.pgm", ts);
 		fout = fopen(fname, "wb");
 		fputs("P5\n40 80\n255\n", fout);
 		for (times = 3200; times != 0; times--)
@@ -235,12 +237,12 @@ int xtract()
 			 if(low > 15) printf("COMPUTATION ERROR!!!  FILE OUTPUT WILL BE INVALID.");
 			 fprintf(fout, " %i", low);
 			 fprintf(fout, " %i", high); */
-			if (buffer == (char)0xe1) {
-				char nextBuf = 0;
+			if (buffer == 0xe1) {
+				unsigned char nextBuf = 0;
 				fread(&nextBuf, 1, 1, fin);
-				if (nextBuf == (char)0xe1) {
+				if (nextBuf == 0xe1) {
 					fputc(0, fout);
-					printf("\n%i.pgm %d: padded by 0", ts, 3200 - times);
+					printf("\n%li.pgm %d: padded by 0", ts, 3200 - times);
 					fseek(fin, -2, SEEK_CUR);
 				}
 				else {
@@ -271,7 +273,7 @@ int xtract()
 	return 0;
 }
 
-int splitm(char* rawfile)
+int splitColorXp(char* rawfile)
 {
 	FILE * fin;
 	FILE * foutr;
@@ -299,8 +301,9 @@ int splitm(char* rawfile)
 	return 0;
 }
 
-int addhColor(long frmSize, size_t seekPos)
+int addhColorXp(long frmSize, long audioSizePerFrm, size_t seekPos)
 {
+	UNREFERENCED_PARAMETER(seekPos);
 	FILE* fin;
 	FILE* fout;
 	long finSize;
@@ -321,7 +324,7 @@ int addhColor(long frmSize, size_t seekPos)
 	fout = fopen("input-right2.wav", "wb");
 	fwrite(riff, 4, 1, fout);
 	fseek(fin, 0, 2);
-	finSize = frmSize * 1960;
+	finSize = frmSize * audioSizePerFrm;
 	fseek(fin, 0, 0);
 	long finSizea;
 	finSizea = finSize + 40;
@@ -339,9 +342,8 @@ int addhColor(long frmSize, size_t seekPos)
 	fwrite(&finSize, 4, 1, fout);
 
 	long tt = 0;
-	for (tt = finSize / 1960; tt > 0; tt--)
-	{
-		for (int i = 0; i < 1960; i++) {
+	for (tt = frmSize; tt > 0; tt--) {
+		for (int i = 0; i < audioSizePerFrm; i++) {
 			fread(&buffer, 1, 1, fin);
 			fwrite(&buffer, 1, 1, fout);
 		}
@@ -352,65 +354,12 @@ int addhColor(long frmSize, size_t seekPos)
 	return 0;
 }
 
-int addhXp(long frmSize, size_t seekPos)
-{
-	FILE* fin;
-	FILE* fout;
-	long finSize;
-	char buffer;
-	fin = fopen("input-raw-right2.raw", "rb");
-	if (!fin) { printf("ERROR!"); exit(2); }
-	char riff[] = "RIFF";
-	char wave[] = "WAVE";
-	char fmt[] = "fmt ";
-	long sc1size = 16;
-	int audiofmt = 1;
-	int numchanels = 2;
-	long smprate = 17640;
-	long byterate = 35280;
-	int blkalign = 1;
-	int bits = 8;
-	char dataa[] = "data";
-	fout = fopen("input-right2.wav", "wb");
-	fwrite(riff, 4, 1, fout);
-	fseek(fin, 0, 2);
-	finSize = frmSize * 1976;
-	fseek(fin, 0, 0);
-	long finSizea;
-	finSizea = finSize + 40;
-	fwrite(&finSizea, 4, 1, fout);
-	fwrite(wave, 4, 1, fout);
-	fwrite(fmt, 4, 1, fout);
-	fwrite(&sc1size, 4, 1, fout);
-	fwrite(&audiofmt, 2, 1, fout);
-	fwrite(&numchanels, 2, 1, fout);
-	fwrite(&smprate, 4, 1, fout);
-	fwrite(&byterate, 4, 1, fout);
-	fwrite(&blkalign, 2, 1, fout);
-	fwrite(&bits, 2, 1, fout);
-	fwrite(dataa, 4, 1, fout);
-	fwrite(&finSize, 4, 1, fout);
-	
-	long tt = 0;
-	for (tt = finSize / 1976; tt > 0; tt--)
-	{
-		for (int i = 0; i < 1976; i++) {
-			fread(&buffer, 1, 1, fin);
-			fwrite(&buffer, 1, 1, fout);
-		}
-	}
-	fclose(fin);
-	fclose(fout);
-	remove("input-raw-right2.raw");
-	return 0;
-}
-
-int isColor(char* buf)
+int isColor(unsigned char* buf)
 {
 	int nRet = 0;
-	if (buf[0] == (char)0x81 || buf[1] == (char)0xe3 || buf[2] == (char)0xe3 ||
-		buf[3] == (char)0xc7 || buf[4] == (char)0xc7 || buf[5] == (char)0x81 ||
-		buf[6] == (char)0x81 || buf[7] == (char)0xe3 || buf[8] == (char)0xc7) {
+	if (buf[0] == 0x81 || buf[1] == 0xe3 || buf[2] == 0xe3 ||
+		buf[3] == 0xc7 || buf[4] == 0xc7 || buf[5] == 0x81 ||
+		buf[6] == 0x81 || buf[7] == 0xe3 || buf[8] == 0xc7) {
 		nRet = 1;
 	}
 	return nRet;
@@ -420,7 +369,7 @@ int isColor(char* buf)
 size_t fseekToColor(FILE* fp)
 {
 	size_t size = 0;
-	char buf[9] = {};
+	unsigned char buf[9] = {};
 	while (!feof(fp) && !ferror(fp)) {
 		size += fread(buf, 1, 9, fp);
 		if (isColor(buf)) {
@@ -433,14 +382,53 @@ size_t fseekToColor(FILE* fp)
 	return size;
 }
 
-int xtractColor(bool isGray, long* frmSize, size_t* seekPos)
+int drawRGB(unsigned char* video, int* idx, long ts)
+{
+	char fname[20];
+	FILE* fout;
+	char writeBuf[12] = {};
+	int nextline = 108;
+
+	printf("\rProcessing frame number %li", ts);
+	sprintf(fname, "%li.ppm", ts);
+	fout = fopen(fname, "wb");
+	fputs("P6\n144 80\n255\n", fout);
+
+	int nRoop = 0;
+	//  https://forum.videohelp.com/threads/123262-converting-video-formats-(For-Hasbro-s-VideoNow)-I-know-the/page17#post1150041
+	for (int i = 0; i < 17280 / 6; i++) {
+		writeBuf[0] = video[*idx] & 0x0f | video[*idx] << 4 & 0xf0;
+		writeBuf[1] = video[*idx + nextline] & 0x0f | video[*idx + nextline] << 4 & 0xf0;
+		writeBuf[2] = video[*idx + nextline] & 0xf0 | video[*idx + nextline] >> 4 & 0x0f;
+
+		writeBuf[3] = video[*idx + nextline + 1] & 0x0f | video[*idx + nextline + 1] << 4 & 0xf0;
+		writeBuf[4] = video[*idx] & 0xf0 | video[*idx] >> 4 & 0x0f;
+		writeBuf[5] = video[*idx + 1] & 0x0f | video[*idx + 1] << 4 & 0xf0;
+
+		writeBuf[6] = video[*idx + 1] & 0xf0 | video[*idx] >> 4 & 0x0f;
+		writeBuf[7] = video[*idx + nextline + 1] & 0xf0 | video[*idx + nextline + 1] >> 4 & 0x0f;
+		writeBuf[8] = video[*idx + nextline + 2] & 0x0f | video[*idx + nextline + 2] << 4 & 0xf0;
+
+		writeBuf[9] = video[*idx + nextline + 2] & 0xf0 | video[*idx + nextline + 2] >> 4 & 0x0f;
+		writeBuf[10] = video[*idx + 2] & 0x0f | video[*idx + 2] << 4 & 0xf0;
+		writeBuf[11] = video[*idx + 2] & 0xf0 | video[*idx + 2] >> 4 & 0x0f;
+		fwrite(writeBuf, sizeof(writeBuf), 1, fout);
+
+		*idx += 3;
+		if (++nRoop == 36) {
+			*idx += nextline;
+			nRoop = 0;
+		}
+	}
+	fclose(fout);
+	return 0;
+}
+
+int xtractColor(long* frmSize, size_t* seekPos)
 {
 	FILE * fin;
-	FILE * fout;
-	char buffer;
 	long size;
 	long tt;
-	char fname[20];
 	long ts = 0;
 	
 	fin = fopen("input-raw-left2.raw", "rb");
@@ -448,35 +436,21 @@ int xtractColor(bool isGray, long* frmSize, size_t* seekPos)
 	fseek(fin, 0, 2);
 	size = ftell(fin);
 	rewind(fin);
+	unsigned char* buf = (unsigned char*)malloc((size_t)size);
+	if (!buf) exit(1);
+	fread(buf, (size_t)size, 1, fin);
+	rewind(fin);
 	*seekPos = fseekToColor(fin) - 360;
-	printf("Seek size: %d\n", *seekPos);
-	size = size - *seekPos;
+	printf("Seek size: %zd\n", *seekPos);
+	size = (long)(size - *seekPos);
 
 	*frmSize = size / 17640;
+	unsigned char* video = &buf[*seekPos];
+	int idx = 0;
 	for (tt = *frmSize; tt > 0; tt--)
 	{
-		ts += 1;
-		printf("\rProcessing frame number %li", ts);
-		if (isGray) {
-			sprintf(fname, "%i.pgm", ts);
-		}
-		else {
-			sprintf(fname, "%i.ppm", ts);
-		}
-		fout = fopen(fname, "wb");
-		if (isGray) {
-			fputs("P5\n108 160\n255\n", fout);
-		}
-		else {
-			fputs("P6\n36 160\n255\n", fout);
-		}
-		
-		for (int i = 0; i < 17280; i++) {
-			fread(&buffer, 1, 1, fin);
-			fwrite(&buffer, 1, 1, fout);
-		}
-		fclose(fout);
-		fseek(fin, 360, SEEK_CUR);
+		idx += 360;
+		drawRGB(video, &idx, ++ts);
 	}
 	printf("\n");
 	fclose(fin);
@@ -487,7 +461,7 @@ int xtractColor(bool isGray, long* frmSize, size_t* seekPos)
 size_t fseekToXp(FILE* fp)
 {
 	size_t size = 0;
-	char buf[9] = {};
+	unsigned char buf[9] = {};
 	while (!feof(fp) && !ferror(fp)) {
 		size += fread(buf, 1, 9, fp);
 		if (isColor(buf)) {
@@ -500,14 +474,11 @@ size_t fseekToXp(FILE* fp)
 	return size;
 }
 
-int xtractXp(bool isGray, long* frmSize, size_t* seekPos)
+int xtractXp(long* frmSize, size_t* seekPos)
 {
 	FILE * fin;
-	FILE * fout;
-	char buffer;
 	long size;
 	long tt;
-	char fname[20];
 	long ts = 0;
 
 	fin = fopen("input-raw-left2.raw", "rb");
@@ -515,35 +486,21 @@ int xtractXp(bool isGray, long* frmSize, size_t* seekPos)
 	fseek(fin, 0, 2);
 	size = ftell(fin);
 	rewind(fin);
+	unsigned char* buf = (unsigned char*)malloc((size_t)size);
+	if (!buf) exit(1);
+	fread(buf, (size_t)size, 1, fin);
+	rewind(fin);
 	*seekPos = fseekToXp(fin) - 504;
-	printf("Seek size: %d\n", *seekPos);
-	size = size - *seekPos;
+	printf("Seek size: %zd\n", *seekPos);
+	size = (long)(size - *seekPos);
 
 	*frmSize = size / 17784;
+	unsigned char* video = &buf[*seekPos];
+	int idx = 0;
 	for (tt = *frmSize; tt > 0; tt--)
 	{
-		ts += 1;
-		printf("\rProcessing frame number %li", ts);
-		if (isGray) {
-			sprintf(fname, "%i.pgm", ts);
-		}
-		else {
-			sprintf(fname, "%i.ppm", ts);
-		}
-		fout = fopen(fname, "wb");
-		if (isGray) {
-			fputs("P5\n108 160\n255\n", fout);
-		}
-		else {
-			fputs("P6\n36 160\n255\n", fout);
-		}
-
-		for (int i = 0; i < 17280; i++) {
-			fread(&buffer, 1, 1, fin);
-			fwrite(&buffer, 1, 1, fout);
-		}
-		fclose(fout);
-		fseek(fin, 504, SEEK_CUR);
+		idx += 504;
+		drawRGB(video, &idx, ++ts);
 	}
 	printf("\n");
 	fclose(fin);
@@ -553,20 +510,16 @@ int xtractXp(bool isGray, long* frmSize, size_t* seekPos)
 
 int main(int argc, char *argv[])
 {
-	if (argc != 3 && argc != 4) {
+	if (argc != 3) {
 		printf(
 			"This is a collection of tools to decode VideoNow PVD discs.\n"
 			"Usage\n"
 			"\t PVDTools.exe bw <raw file>\n"
 			"\t\t Output pgm (40 x 80 gray scale) and wav file\n"
 			"\t PVDTools.exe color <raw file>\n"
-			"\t\t Output ppm (36 x 160 incomplete color) and wav file\n"
-			"\t PVDTools.exe color <raw file> /g\n"
-			"\t\t Output pgm (108 x 160 gray scale) and wav file\n"
+			"\t\t Output ppm (144 x 80 full color) and wav file\n"
 			"\t PVDTools.exe xp <raw file>\n"
-			"\t\t Output ppm (36 x 160 incomplete color) and wav file\n"
-			"\t PVDTools.exe xp <raw file> /g\n"
-			"\t\t Output pgm (108 x 160 gray scale) and wav file\n"
+			"\t\t Output ppm (144 x 80 full color) and wav file\n"
 		);
 	}
 	else {
@@ -579,24 +532,14 @@ int main(int argc, char *argv[])
 			xtract();
 		}
 		else if (!strcmp(argv[1], "color")) {
-			splitm(argv[2]);
-			if (argc == 4 && !strncmp(argv[3], "/g", 2)) {
-				xtractColor(true, &frmSize, &seekPos);
-			}
-			else {
-				xtractColor(false, &frmSize, &seekPos);
-			}
-			addhColor(frmSize, seekPos);
+			splitColorXp(argv[2]);
+			xtractColor(&frmSize, &seekPos);
+			addhColorXp(frmSize, 1960, seekPos);
 		}
 		else if (!strcmp(argv[1], "xp")) {
-			splitm(argv[2]);
-			if (argc == 4 && !strncmp(argv[3], "/g", 2)) {
-				xtractXp(true, &frmSize, &seekPos);
-			}
-			else {
-				xtractXp(false, &frmSize, &seekPos);
-			}
-			addhXp(frmSize, seekPos);
+			splitColorXp(argv[2]);
+			xtractXp(&frmSize, &seekPos);
+			addhColorXp(frmSize, 1976, seekPos);
 		}
 	}
 	return 0;
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # PVDTools
 
-This is a collection of tools to decode VideoNow PVD discs.  Below are instructions.
+This is a collection of tools to decode VideoNow PVD discs.
 
 Original binary and source code is [here](https://sourceforge.net/projects/pvdtools/)  
 Technical documentation about black & white disc is [here](https://web.archive.org/web/20161026023116/http://pvdtools.sourceforge.net:80/format.txt)  
@@ -14,37 +14,11 @@ Technical documentation about color disc is [here](https://www.videohelp.com/for
    PVDTools.exe color [binary file]  
    => You can get 1 wav file and multiple ppm files in the current directory.
    
-   PVDTools.exe color [binary file] /g  
-   => You can get 1 wav file and multiple pgm files in the current directory.
-   
    PVDTools.exe xp [binary file]  
    => You can get 1 wav file and multiple ppm files in the current directory.
 
-   PVDTools.exe xp [binary file] /g  
-   => You can get 1 wav file and multiple pgm files in the current directory.
-
    ** binary file is 2352 bytes per sector. You can generate it by [DiscImageCreator](https://github.com/saramibreak/DiscImageCreator)  
    
-2. Get [irfanview](https://www.irfanview.com/) if you don't have it already
-3. Select Batch Convert/Rename from the file menu.
-4. Select all of the extracted frames(hint:  hold down the shift key and select the first and last displayed)
-5. Click add
-6. Set "Work as:" to Batch conversion
-7. Set Output format to "BMP"
-8. Check(tick) "Use advanced options"
-9. Check(tick) "Resize"
-10. Uncheck(untick) "Preserve aspect ratio"
-11. Be sure that "Use Resample function" is ticked
-12. Type in 80 for Height and Width (color or xp disc is 160 height and 108 width)
-13. Check(tick) "Change color depth"
-14. Select "16.7 Million colors(24BPP)"
-15. Check(tick) "Delete original files after conversion"
-16. Select an output directory
-17. Click Start
-18. When IrfanView is done click exit and close IrfanView
-19. Open [VirtualDub](http://www.virtualdub.org/)
-20. Select File>Open and open 1.bmp
-21. Select Video>Frame Rate and select "Change so video and audio durations match"
-22. Click OK
-23. Select Audio>WAV Audio and open input-right2.wav in the same directory as the original wav file.
-24. If you want to convert avi, Select File>Save as AVI.
+2. Get [irfanview](https://www.irfanview.com/) and [VirtualDub](http://www.virtualdub.org/) if you don't have it yet
+3. Edit !PpmToBmpToAvi.bat in accordance with your environment
+4. Execute !PpmToBmpToAvi.bat
